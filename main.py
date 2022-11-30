c8c19ab5f15a2eb2fb8744aa596dbdd0d7128ff8
Thomas Boutry <thomas.boutry@x3rus.com>
2022-11-29 22:58:07 -0500

    Split main application into the classe mySuperApp

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 #
 
 from configparser import ConfigParser
+import mySuperApp
 
 
-config_parser = ConfigParser()
-config_parser.read('toto.ini')
+#config_parser = ConfigParser()
+#config_parser.read('toto.ini')
 
 
-print(config_parser['camunda']['base_url'])
+#print(config_parser['camunda']['base_url'])
+
+
+
+if __name__ == "__main__":
+    app = mySuperApp.mySuperApp(None)
+    app.run_or_app_action()
 
---
+++ mySuperApp.py
@@ -0,0 +1,16 @@
+
+class mySuperApp:
+    """ My will safe the word """
+    def __init__(self, my_setting):
+        """ Initialisation of the class
+
+        param : my_setting  is an Setting pydandic class
+                already validated
+
+        """
+        self.setting = my_setting
+
+    def run_or_app_action(self):
+        """ start of the app the "real" start with settings """
+        print("my_super_app.run_or_app_action , let's the show begging")
+        pass
